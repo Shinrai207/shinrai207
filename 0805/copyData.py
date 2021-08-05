a6a9da392b92a451a80dd8d63e9f7d4ebee29581
チャン チュン ティン <210115@yse.ac.jp>
2021-08-05 13:53:40 +0900

    20210805: openpyxl code

--- 0805/copyData.py
+++ 0805/copyData.py
@@ -1,5 +1,13 @@
 import openpyxl
 wb = openpyxl.load_workbook("0805.xlsx")
-ws_copy = wb.copy_worksheet(wb["hello"])
-
+ws_hello = wb["hello"]
+ws_hello2 = wb["hello2"]
+  
+for i in range (1, 3):
+    for j in range (1, 9):
+      
+        c = ws_hello.cell(row = i, column = j)
+  
+        ws_hello2.cell(row = i, column = j).value = c.value
+  
 wb.save("0805.xlsx")
